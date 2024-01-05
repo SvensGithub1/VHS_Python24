382ff82d2f84acca6eb632b0ca7fd44e8833ec08
SvensGithub1 <60943193+SvensGithub1@users.noreply.github.com>
2024-01-05 11:53:16 +0100

    Add files via upload

---
+++ greet_user.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 10:30:26 2024
+
+@author: Sven
+"""
+
+# Get the user's name
+user_name = input("Enter your name: ")
+
+# Greet the user
+print("Hello, " + user_name + "! Nice to meet you.")
